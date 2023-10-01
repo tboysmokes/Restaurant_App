b0d4e745d5f9d0eadc16af5d49769725d69b9fc7
olododo omotomiwa <omotomiwar@gmail.com>
2023-10-01 11:09:31 +0100

    fixed the input error if noting is enter in the app or there are just spaces

--- app.py
+++ app.py
@@ -9,20 +9,26 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'newproject'
 
 def get_data():
+   
+   # yelp API endpoint URL
    url = 'https://api.yelp.com/v3/businesses/search'
 
+   # authorization header (API KEY)
    hearders = {
        'Authorization': f'Bearer {os.getenv("API_KEY")}'
    }
 
+   # define search parameters
    params = {
        'term': 'restaurant',
        'location': 'lagos',
        'limit': 10
    }
 
+   # send GET request to yelp API with the parameters
    respond = requests.get(url, params=params, hearders=hearders)
 
+   #check if request was successful (status code 200)
    if respond.status_code == 200:
        data = respond.json()
    print(data)
